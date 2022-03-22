c6efad3ebae10782e02b81c5012b204a9b9c166f
David Atherton <davidatherton92@gmail.com>
2022-03-22 21:03:59 +0000

    Add boundary class

--- coding-challenges/2d-raycasting/sketch.js
+++ coding-challenges/2d-raycasting/sketch.js
@@ -1,9 +1,17 @@
+/* 
+ * 2D Raycasting
+ */
+
+let boundary;
+
 function setup()
 {
   createCanvas(500, 500);
+  boundary = new Boundary(0, 0, width, height);
 }
 
 function draw()
 {
   background(0);
+  boundary.draw();
 }
